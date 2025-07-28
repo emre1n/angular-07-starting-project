85560f4e757700b4ec5dfdb0e0d1c85dc10066cc
emre1n <emre1n@protonmail.com>
2025-07-28 14:31:52 +0300

    implement updateTaskStatus method to task-item component

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, signal } from '@angular/core';
 
-import { Task } from './task.model';
+import type { Task, TaskStatus } from './task.model';
 
 @Injectable({
   providedIn: 'root',
@@ -18,5 +18,13 @@ export class TaskService {
     };
 
     this.tasks.update((oldTasks) => [...oldTasks, newTask]);
+  }
+
+  updateTaskStatus(taskId: string, newStatus: TaskStatus) {
+    this.tasks.update((oldTasks) =>
+      oldTasks.map((task) =>
+        task.id === taskId ? { ...task, status: newStatus } : task
+      )
+    );
   }
 }
